dbd1df5815459dc08162d70e00348dfa4807dfe2
Satyaranjan Mohapatra <satyaranjanmohapatra@rocketmail.com>
2020-06-05 15:57:39 +0530

    Initial Commit

---
+++ AssessmentAIB_WeatherForcast_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssessmentAIB_WeatherForcast_1
+//
+//  Created by SatyaRanjan Mohapatra on 05/06/20.
+//  Copyright © 2020 SatyaranjanMohapatra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
